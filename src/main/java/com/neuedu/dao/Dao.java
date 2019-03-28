3cef19daf80b459af8f998b9d59e0b5ffd415345
MaiBenBen <873297660@qq.com>
2019-03-28 16:48:14 +0800

    测试

---
+++ src/main/java/com/neuedu/dao/Dao.java
@@ -0,0 +1,7 @@
+package com.neuedu.dao;
+
+/**
+ * Created by MaiBenBen on 2019/3/28.
+ */
+public class Dao {
+}
---
+++ src/main/java/com/neuedu/service/Service.java
@@ -0,0 +1,7 @@
+package com.neuedu.service;
+
+/**
+ * Created by MaiBenBen on 2019/3/28.
+ */
+public class Service {
+}
